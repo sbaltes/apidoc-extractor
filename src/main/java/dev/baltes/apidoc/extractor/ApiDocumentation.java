1b67a98609e397c8877dfe071007dee2e366b909
Wesley Tan <wesleytws2018@gmail.com>
2021-03-19 18:59:01 +1030

    Add more annotation types

--- src/main/java/dev/baltes/apidoc/extractor/ApiDocumentation.java
+++ src/main/java/dev/baltes/apidoc/extractor/ApiDocumentation.java
@@ -1,10 +1,24 @@
 package dev.baltes.apidoc.extractor;
 
 class ApiDocumentation {
-    String method;
-    String path;
-    String documentation;
-    String notes;
+    private String filename;
+    private String repo;
+    private String file;
+    private String classpath;
+    private String method;
+    private String path;
+    private String documentation;
+    private String notes;
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+        this.repo = filename.split("#")[0].replace("$", "/");
+        this.file = filename.split("#")[1].replace("$", "/");
+    }
+
+    public void setClasspath(String classpath) {
+        this.classpath = classpath;
+    }
 
     public void setMethod(String method) {
         this.method = method;
@@ -14,10 +28,10 @@ class ApiDocumentation {
         this.path = path;
     }
 
-    void setDocumentation(String documentation) {
+    public void setDocumentation(String documentation) {
         if (this.documentation == null) {
             this.documentation = documentation;
-        } else {
+        } else if (!this.documentation.equals(documentation)) {
             this.documentation += " + " + documentation;
         }
     }
@@ -26,35 +40,55 @@ class ApiDocumentation {
         this.notes = notes;
     }
 
+    public String getFilename() {
+        return filename;
+    }
+
+    public String getClasspath() {
+        return (classpath == null || clean(classpath).isBlank()) ? "" : clean(classpath);
+    }
+
     public String getMethod() {
-        return method == null ? "n/a" : method;
+        return (method == null || clean(method).isBlank()) ? "n/a" : clean(method);
     }
 
     public String getPath() {
-        return path == null ? "n/a" : clean(path);
+        return (path == null || clean(path).isBlank()) ? "" : clean(path);
     }
 
     public String getDocumentation() {
-        return documentation == null ? "n/a" : clean(documentation);
-
+        return (documentation == null || clean(documentation).isBlank()) ? "n/a" : clean(documentation);
     }
 
     public String getNotes() {
-        return notes == null ? "n/a" : clean(notes);
+        return (notes == null || clean(notes).isBlank()) ? "n/a" : clean(notes);
+    }
+
+    private String getFullpath() {
+        if (getPath().isBlank()) return "n/a";
+        if (getClasspath().isBlank()) return getPath();
+        return String.format("%s/%s", getClasspath(), getPath()).replaceAll("/{2,}", "/");
     }
 
     @Override
     public String toString() {
-        return String.format("\"%s\",\"%s\",\"%s\",\"%s\"", getMethod(), getPath(), getDocumentation(), getNotes());
+        return String.format("\"%s\",\"%s\",\"%s\",\"%s\",\"%s\",\"%s\"",
+                repo, file, getMethod(), getFullpath(), getDocumentation(), getNotes());
     }
 
     private String clean(String str) {
-        return str.replaceAll("\"\\s*\\+\\s*\"", " ")
+        return str.replaceAll("(?<!\\\\)((?:\\\\{2})*)\"\\s*\\+\\s*\"", "$1")
+                .replaceAll("(?<!\\\\)((?:\\\\{2})*)\"\\s*\\+\\s*(?!\")", "$1 ")
+                .replaceAll("(?<!\")\\s*\\+\\s*\"", " ")
                 .replaceAll("^\"", "")
-                .replaceAll("\"$", "");
+                .replaceAll("\"$", "")
+                .replace("\"", "\"\"");
     }
 
     public boolean isEmpty() {
-        return method == null && path == null && (documentation == null || notes == null);
+        return getMethod().equals("n/a")
+                && getFullpath().equals("n/a")
+                && getDocumentation().equals("n/a")
+                && getNotes().equals("n/a");
     }
 }
--- src/main/java/dev/baltes/apidoc/extractor/Main.java
+++ src/main/java/dev/baltes/apidoc/extractor/Main.java
@@ -1,89 +1,261 @@
 package dev.baltes.apidoc.extractor;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Properties;
+import java.util.stream.Stream;
+
 import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.expr.AnnotationExpr;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Properties;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Reading properties...");
+
+        // System.out.println("Reading properties...");
         Properties properties = new Properties();
-        try {
-            properties.load(new FileInputStream("config.properties"));
+        try (FileInputStream config = new FileInputStream("config.properties")) {
+            properties.load(config);
         } catch (IOException e) {
             e.printStackTrace();
         }
+
         String inputDirectory = properties.getProperty("InputDirectory");
-        System.out.printf("Reading Java files from directory '%s'...\n", inputDirectory);
-        System.out.println("Extracting API documentation...");
-        try {
-            Files.list(Paths.get(inputDirectory)).forEach(
-                    file -> {
-                        try {
-                            CompilationUnit cu  = StaticJavaParser.parse(file);
-                            new MethodAnnotationVisitor().visit(cu, null);
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-            );
+        // System.out.printf("Reading Java files from directory '%s'...%n", inputDirectory);
+        try (Stream<Path> files = Files.list(Paths.get(inputDirectory))) {
+
+            // System.out.println("Extracting API documentation...");
+            System.out.println("\"repo\",\"file\",\"method\",\"path\",\"documentation\",\"notes\"");
+
+            files.forEach(file -> {
+                try {
+                    ApiDocumentation apiDocumentation = new ApiDocumentation();
+                    apiDocumentation.setFilename(file.getFileName().toString());
+
+                    CompilationUnit cu = StaticJavaParser.parse(file);
+                    new ClassAnnotationVisitor().visit(cu, apiDocumentation);
+                    new MethodAnnotationVisitor().visit(cu, apiDocumentation);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private static class MethodAnnotationVisitor extends VoidVisitorAdapter<Void> {
+    private static class ClassAnnotationVisitor extends VoidVisitorAdapter<ApiDocumentation> {
         @Override
-        public void visit(final MethodDeclaration n, final Void arg) {
-            ApiDocumentation apiDocumentation = new ApiDocumentation();
-            //System.out.printf("Visiting method %s...\n", n.getName());
-            for (AnnotationExpr annotation: n.getAnnotations()) {
-                //System.out.printf("Visiting annotation %s...\n", annotation.toString());
-                annotation.toAnnotationExpr().ifPresent(
-                        annotationExpr -> {
-                            switch (annotationExpr.getName().asString()) {
-                                case "GET":
-                                    apiDocumentation.setMethod("GET");
-                                    break;
-                                case "POST":
-                                    apiDocumentation.setMethod("POST");
-                                    break;
-                                case "PUT":
-                                    apiDocumentation.setMethod("PUT");
-                                    break;
-                                case "DELETE":
-                                    apiDocumentation.setMethod("DELETE");
-                                    break;
-                                case "Path":
-                                    annotationExpr.toSingleMemberAnnotationExpr().ifPresent(
-                                            singleMemberAnnotationExpr -> apiDocumentation.setPath(singleMemberAnnotationExpr.getMemberValue().toString())
-                                    );
-                                    break;
-                                case "ApiOperation":
-                                    annotationExpr.toNormalAnnotationExpr().ifPresent(
-                                           normalAnnotationExpr -> normalAnnotationExpr.getPairs().forEach(
-                                                   pair -> {
-                                                       if (pair.getName().toString().equals("value")) {
-                                                           apiDocumentation.setDocumentation(pair.getValue().toString());
-                                                       } else if (pair.getName().toString().equals("notes")) {
-                                                           apiDocumentation.setNotes(pair.getValue().toString());
-                                                       }
-                                                   }
-                                           )
-                                    );
-                                    break;
+        public void visit(final ClassOrInterfaceDeclaration n, final ApiDocumentation arg) {
+
+            for (AnnotationExpr annotation : n.getAnnotations()) {
+                annotation.toAnnotationExpr().ifPresent(annotationExpr -> {
+
+                    // Api:
+                    // https://docs.swagger.io/swagger-core/v1.5.X/apidocs/io/swagger/annotations/Api.html
+                    if (annotationExpr.getName().asString().equals("Api")) {
+                        annotationExpr.toSingleMemberAnnotationExpr().ifPresent(singleMemberAnnotationExpr -> {
+                            if (!singleMemberAnnotationExpr.getMemberValue().toString().contains(" ")) {
+                                arg.setClasspath(singleMemberAnnotationExpr.getMemberValue().toString());
                             }
-                        }
-                );
+                        });
+
+                        annotationExpr.toNormalAnnotationExpr()
+                                .ifPresent(normalAnnotationExpr -> normalAnnotationExpr.getPairs().forEach(pair -> {
+                                    if (!pair.getValue().toString().contains(" ")
+                                            && (pair.getName().toString().equals("basePath")
+                                                    || pair.getName().toString().equals("value"))) {
+                                        arg.setClasspath(pair.getValue().toString());
+                                    }
+                                }));
+                    }
+                });
             }
+
+            for (AnnotationExpr annotation : n.getAnnotations()) {
+                annotation.toAnnotationExpr().ifPresent(annotationExpr -> {
+
+                    switch (annotationExpr.getName().asString()) {
+
+                    // RequestMapping:
+                    // https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/web/bind/annotation/RequestMapping.html
+                    case "RequestMapping":
+                        annotationExpr.toSingleMemberAnnotationExpr().ifPresent(singleMemberAnnotationExpr -> arg
+                                .setClasspath(singleMemberAnnotationExpr.getMemberValue().toString()));
+
+                        annotationExpr.toNormalAnnotationExpr()
+                                .ifPresent(normalAnnotationExpr -> normalAnnotationExpr.getPairs().forEach(pair -> {
+                                    if (pair.getName().toString().equals("path")
+                                            || pair.getName().toString().equals("value")) {
+                                        arg.setClasspath(pair.getValue().toString());
+                                    }
+                                }));
+                        break;
+
+                    // Path:
+                    // https://docs.oracle.com/javaee/7/api/javax/ws/rs/Path.html
+                    case "Path":
+                    case "javax.ws.rs.Path":
+                        annotationExpr.toSingleMemberAnnotationExpr().ifPresent(singleMemberAnnotationExpr -> arg
+                                .setClasspath(singleMemberAnnotationExpr.getMemberValue().toString()));
+
+                        annotationExpr.toNormalAnnotationExpr()
+                                .ifPresent(normalAnnotationExpr -> normalAnnotationExpr.getPairs().forEach(pair -> {
+                                    if (pair.getName().toString().equals("value")) {
+                                        arg.setClasspath(pair.getValue().toString());
+                                    }
+                                }));
+                        break;
+
+                    default:
+                        break;
+                    }
+                });
+            }
+        }
+    }
+
+    private static class MethodAnnotationVisitor extends VoidVisitorAdapter<ApiDocumentation> {
+        @Override
+        public void visit(final MethodDeclaration n, final ApiDocumentation arg) {
+
+            ApiDocumentation apiDocumentation = new ApiDocumentation();
+
+            apiDocumentation.setFilename(arg.getFilename());
+            apiDocumentation.setClasspath(arg.getClasspath());
+
+            // System.out.printf("Visiting method %s...%n", n.getName());
+            for (AnnotationExpr annotation : n.getAnnotations()) {
+                // System.out.printf("Visiting annotation %s...%n", annotation.toString());
+                annotation.toAnnotationExpr().ifPresent(annotationExpr -> {
+
+                    String annotationName = annotationExpr.getName().asString();
+
+                    switch (annotationName) {
+
+                    // Methods:
+                    // https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/web/bind/annotation/RequestMethod.html
+                    case "GET":
+                    case "HEAD":
+                    case "POST":
+                    case "PUT":
+                    case "DELETE":
+                    case "OPTIONS":
+                    case "PATCH":
+                    case "TRACE":
+                        apiDocumentation.setMethod(annotationName);
+                        break;
+
+                    // Mappings:
+                    // https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/web/bind/annotation/GetMapping.html
+                    case "GetMapping":
+                    case "PostMapping":
+                    case "PutMapping":
+                    case "DeleteMapping":
+                    case "PatchMapping":
+                        apiDocumentation.setMethod(annotationName.replace("Mapping", "").toUpperCase());
+
+                        annotationExpr.toSingleMemberAnnotationExpr()
+                                .ifPresent(singleMemberAnnotationExpr -> apiDocumentation
+                                        .setPath(singleMemberAnnotationExpr.getMemberValue().toString()));
+
+                        annotationExpr.toNormalAnnotationExpr()
+                                .ifPresent(normalAnnotationExpr -> normalAnnotationExpr.getPairs().forEach(pair -> {
+                                    if (pair.getName().toString().equals("path")
+                                            || pair.getName().toString().equals("value")) {
+                                        apiDocumentation.setPath(pair.getValue().toString());
+                                    }
+                                }));
+                        break;
+
+                    // RequestMapping:
+                    // https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/web/bind/annotation/RequestMapping.html
+                    case "RequestMapping":
+                        annotationExpr.toSingleMemberAnnotationExpr()
+                                .ifPresent(singleMemberAnnotationExpr -> apiDocumentation
+                                        .setPath(singleMemberAnnotationExpr.getMemberValue().toString()));
+
+                        annotationExpr.toNormalAnnotationExpr()
+                                .ifPresent(normalAnnotationExpr -> normalAnnotationExpr.getPairs().forEach(pair -> {
+                                    if (pair.getName().toString().equals("method")) {
+                                        String[] methods = pair.getValue().toString()
+                                                .replaceAll("\\{ |RequestMethod.| \\}", "").split(", ");
+                                        Arrays.sort(methods);
+                                        apiDocumentation.setMethod(String.join(", ", methods));
+                                    } else if (pair.getName().toString().equals("path")
+                                            || pair.getName().toString().equals("value")) {
+                                        apiDocumentation.setPath(pair.getValue().toString());
+                                    }
+                                }));
+                        break;
+
+                    // Path:
+                    // https://docs.oracle.com/javaee/7/api/javax/ws/rs/Path.html
+                    case "Path":
+                    case "javax.ws.rs.Path":
+                        annotationExpr.toSingleMemberAnnotationExpr()
+                                .ifPresent(singleMemberAnnotationExpr -> apiDocumentation
+                                        .setPath(singleMemberAnnotationExpr.getMemberValue().toString()));
+
+                        annotationExpr.toNormalAnnotationExpr()
+                                .ifPresent(normalAnnotationExpr -> normalAnnotationExpr.getPairs().forEach(pair -> {
+                                    if (pair.getName().toString().equals("value")) {
+                                        apiDocumentation.setPath(pair.getValue().toString());
+                                    }
+                                }));
+                        break;
+
+                    // Operation:
+                    // https://docs.swagger.io/swagger-core/v2.1.7/apidocs/io/swagger/v3/oas/annotations/Operation.html
+                    case "Operation":
+                        annotationExpr.toSingleMemberAnnotationExpr()
+                                .ifPresent(singleMemberAnnotationExpr -> apiDocumentation
+                                        .setDocumentation(singleMemberAnnotationExpr.getMemberValue().toString()));
+
+                        annotationExpr.toNormalAnnotationExpr()
+                                .ifPresent(normalAnnotationExpr -> normalAnnotationExpr.getPairs().forEach(pair -> {
+                                    if (pair.getName().toString().equals("method")) {
+                                        apiDocumentation.setMethod(pair.getValue().toString());
+                                    } else if (pair.getName().toString().equals("summary")) {
+                                        apiDocumentation.setDocumentation(pair.getValue().toString());
+                                    } else if (pair.getName().toString().equals("description")) {
+                                        apiDocumentation.setNotes(pair.getValue().toString());
+                                    }
+                                }));
+                        break;
+
+                    // ApiOperation:
+                    // https://docs.swagger.io/swagger-core/v1.5.X/apidocs/io/swagger/annotations/ApiOperation.html
+                    case "ApiOperation":
+                        annotationExpr.toSingleMemberAnnotationExpr()
+                                .ifPresent(singleMemberAnnotationExpr -> apiDocumentation
+                                        .setDocumentation(singleMemberAnnotationExpr.getMemberValue().toString()));
+
+                        annotationExpr.toNormalAnnotationExpr()
+                                .ifPresent(normalAnnotationExpr -> normalAnnotationExpr.getPairs().forEach(pair -> {
+                                    if (pair.getName().toString().equals("httpMethod")) {
+                                        apiDocumentation.setMethod(pair.getValue().toString());
+                                    } else if (pair.getName().toString().equals("value")) {
+                                        apiDocumentation.setDocumentation(pair.getValue().toString());
+                                    } else if (pair.getName().toString().equals("notes")) {
+                                        apiDocumentation.setNotes(pair.getValue().toString());
+                                    }
+                                }));
+                        break;
+
+                    default:
+                        break;
+                    }
+                });
+            }
+
             if (!apiDocumentation.isEmpty()) {
                 System.out.println(apiDocumentation);
             }
